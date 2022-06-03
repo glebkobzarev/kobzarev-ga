2ef54d3722096b1031dd2a79c1992427924418dc
Kobzarev GA <transformer1112@mail.ru>
2022-06-03 11:56:00 +0300

    aple

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# Create your views here
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+ 'blog/post/list.html',
+ {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post}, status=Post.Status.PUBLISHED)
+from .models import Post, Comment
+from .forms import CommentForm
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    
+    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'form': form})
